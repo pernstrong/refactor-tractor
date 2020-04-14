28e6a334c878a1f73021ae68fa1735a37cdacde9
Dave Pernitz <pernitz@uwalumni.com>
2020-04-13 19:33:58 -0600

    Fix merge conflicts

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,20 +1,22 @@
+
 import Fit from '../src/Fit';
 
-class Sleep extends Fit {
-  constructor(data, userRepository) {
-    super(data, userRepository)
-    this.hoursSlept = data.hoursSlept;
-    this.sleepQuality = data.sleepQuality;
-    this.sleep(userRepository);
-  }
-
-  sleep(userRepo) {
-    var sleep = this;
-    userRepo.users.find(function(user) {
-      return user.id === sleep.userId;
-    })
-    // return currentUser.updateSleep(this.date, this.hoursSlept, this.sleepQuality);
-  }
+class Sleep extends Fit{
+constructor(data, userRepository) {
+  super(data, userRepository)
+  this.userId = data.userID;
+  this.date = data.date;
+  this.hoursSlept = data.hoursSlept;
+  this.sleepQuality = data.sleepQuality;
+  this.sleep(userRepository);
+}
+sleep(userRepo) {
+  var sleep = this;
+  let currentUser = userRepo.users.find(function(user) {
+    return user.id === sleep.userId;
+  })
+  return currentUser.updateSleep(this.date, this.hoursSlept, this.sleepQuality);
+}
 
 }
 
